dbdb28f1b20bbe4b6d4eb7362818a7b438bb451f
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-06-07 08:07:15 +0200

    Add test

---
+++ test/test-initial-equilibrium.jl
@@ -0,0 +1,12 @@
+using Pkg
+if Base.active_project() != joinpath(@__DIR__, "Project.toml")
+    Pkg.activate(joinpath(@__DIR__))
+end
+
+using Timers
+tic()
+@info "Loading packages..."
+using Test
+using RamAirKite
+using LinearAlgebra
+toc()
