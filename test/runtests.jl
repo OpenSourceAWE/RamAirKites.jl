ab09639e23dcbca5b9b13b15723e26bdd2d52419
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-06-08 23:02:45 +0200

    Add parking test to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using Test
 @testset "RamAirKite.jl" begin
     include("test-data-path.jl")
     include("test-initial-equilibrium.jl")
+    include("test-parking.jl")
     include("test-simulation-disabled.jl")
 end
 nothing
