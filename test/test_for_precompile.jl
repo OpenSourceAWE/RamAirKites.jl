b9b332ef4858e3570c3e5c3b028a886b67fc4c7d
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-06-10 11:26:26 +0200

    Add files for create_sys_image

---
+++ test/test_for_precompile.jl
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Jelle Poland, Bart van de Lint, Uwe Fechner
+# SPDX-License-Identifier: MPL-2.0
+
+using GLMakie
+using RamAirKite
+
+set_data_path(ram_air_data_path())
+set = Settings("system.yaml")
+set.physical_model = "ram"
+set.v_wind = 12.51
+set.upwind_dir = -90.0
+set.l_tether = 25.0
+
+sys_struct = create_sys_struct(set)
+sam = SymbolicAWEModel(set, sys_struct)
+
+init!(sam; remake=false)
+find_steady_state!(sam; dt=0.05, vsm_interval=0)
+
+# Warm up plotting
+fig = Makie.plot(sam.sys_struct)
