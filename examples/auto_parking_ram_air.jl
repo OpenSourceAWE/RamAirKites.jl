0bc2766603e5c296a97beece59f1bba28f9ee486
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-07-01 22:50:17 +0100

    Example now uses yaml file

--- examples/auto_parking_ram_air.jl
+++ examples/auto_parking_ram_air.jl
@@ -74,8 +74,13 @@ set.upwind_dir = UPWIND_DIR
 set.profile_law = PROFILE_LAW
 set.l_tether = TETHER_LENGTH
 
-# 1. system structure
-sys_struct = create_sys_struct(set)
+# 1. system structure (loaded from exported YAML)
+import VortexStepMethod: VSMSettings
+vsm_set_path = joinpath(get_data_path(), "vsm_settings.yaml")
+vsm_set = VSMSettings(vsm_set_path; data_prefix=false)
+sys_struct = load_sys_struct_from_yaml(
+    joinpath(get_data_path(), "ram_air_kite_export.yaml");
+    system_name="ram_air", set=set, vsm_set=vsm_set)
 
 # 2. model
 sam = SymbolicAWEModel(set, sys_struct)
