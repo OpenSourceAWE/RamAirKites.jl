7d4fc95ea1e4c403f43a0e4eef215df67c98c49d
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-07-01 22:57:48 +0100

    Update third example

--- examples/steering_test_ram_air.jl
+++ examples/steering_test_ram_air.jl
@@ -71,8 +71,13 @@ set.profile_law = 3
 set.l_tether = TETHER_LENGTH
 set.sample_freq = Int(round(1 / dt))
 
-# 1. system structure
-sys_struct = create_sys_struct(set)
+# 1. system structure (loaded from exported YAML)
+import VortexStepMethod: VSMSettings
+vsm_set_path = joinpath(get_data_path(), "vsm_settings.yaml")
+vsm_set = VSMSettings(vsm_set_path; data_prefix=false)
+sys_struct = load_sys_struct_from_yaml(
+    joinpath(get_data_path(), "ram_air_kite_export.yaml");
+    system_name="ram_air", set=set, vsm_set=vsm_set)
 
 # 2. model
 sam = SymbolicAWEModel(set, sys_struct)
